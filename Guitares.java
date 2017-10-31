d13f5d68b8a7e73857d896c6d9558e8ffe05d727
comedev <32388830+comedev@users.noreply.github.com>
2017-10-31 21:07:39 +0100

    Update Guitares.java

--- Guitares.java
+++ Guitares.java
@@ -3,6 +3,9 @@ public class Guitares {
     public static void main(String [] args){
         int size = 3;
         Guitare[] array;        //Création du tableau
+      
+        // Pour rajouter des guitares electriques, il n'est pas nécessaire d'appeler la classe GuitareElectrique car elle herite de Guitare
+        // Le principe d'héritage
         array = new Guitare[3];
         array[0] = new Guitare(3,"tin");        //Remplissage du tableau
         array[1] = new Guitare(5,"drin");
